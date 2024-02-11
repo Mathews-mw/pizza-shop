a1efa81b11467ea43e09919ba393973674c5976a
Mathews Araujo <mathews.mw@gmail.com>
2024-02-11 11:55:18 -0400

    first commit

---
+++ src/routes.tsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import { AppLayout } from './pages/_layout/app';
+import { AuthLayout } from './pages/_layout/auth';
+import { Dashboard } from './pages/app/dashboard';
+import { SignIn } from './pages/auth/sign-in';
+import { SignUp } from './pages/auth/sign-up';
+
+export const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <AppLayout />,
+		children: [{ path: '/', element: <Dashboard /> }],
+	},
+	{
+		path: '/',
+		element: <AuthLayout />,
+		children: [
+			{ path: '/sign-in', element: <SignIn /> },
+			{ path: '/sign-up', element: <SignUp /> },
+		],
+	},
+]);
